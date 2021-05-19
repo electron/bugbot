9452ac457926e2190dd5d19573955761da2b391d
clavin <cwatford@slack-corp.com>
2021-05-19 17:30:34 -0600

    fix: remove old files too

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-process.env.SPEC_RUNNING = '1';
-
-module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  preset: 'ts-jest',
-  roots: ['<rootDir>/spec'],
-  testRegex: '(/spec/.*|(\\.|/)(test|spec))\\.tsx?$',
-};
