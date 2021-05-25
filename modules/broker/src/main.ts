7a41599cad038e52261b118659890a4dafe59824
Charles Kerr <charles@charleskerr.com>
2021-05-25 11:26:37 -0500

    refactor: make broker port configurable in env

--- modules/broker/src/main.ts
+++ modules/broker/src/main.ts
@@ -4,6 +4,6 @@ import { Server } from './server';
 const broker = new Broker();
 const server = new Server({
   broker,
-  port: 8088,
+  port: Number.parseInt(process.env.BUGBOT_BROKER_PORT, 10) || 8088,
 });
 server.listen();
