a2a4ba4ea0e92d5e17225be918ee8f98c5d1393a
clavin <cwatford@slack-corp.com>
2021-05-27 06:21:39 -0600

    feat: update runner again

--- modules/runner/src/main.ts
+++ modules/runner/src/main.ts
@@ -3,251 +3,286 @@ import debug from 'debug';
 import got from 'got';
 import { URL } from 'url';
 import { v4 as uuidv4 } from 'uuid';
+import { env } from '@electron/bugbot-shared/lib/env-vars';
 import {
   FiddleBisectResult,
   parseFiddleBisectOutput,
 } from './fiddle-bisect-parser';
-import { env } from '@electron/bugbot-shared/lib/env-vars';
 
 const d = debug('runner');
 
-/* eslint-disable no-use-before-define */
-// ^ I think the file reads a little better in the same order as execution; the
-//   execution is simply broken into separate functions.
-
-type IncomingJob = {
-  id: string;
-  os: string;
+interface BaseJob {
+  client_data?: string;
   gist: string;
+  id: string;
+  os?: 'linux' | 'windows' | 'mac';
+  error?: string;
   runner?: string;
-} & (
-  | {
-      type: 'bisect';
-      first: string;
-      last: string;
-    }
-  | {
-      type: 'test';
-      version: string;
-    }
-);
-
-interface RunnerContext {
-  uuid: string;
-  fiddleExecPath: string;
-  brokerUrl: string;
-  osFilter: string;
-  pollTimeoutMs: number;
+  time_created: number;
+  time_started?: number;
+  time_finished?: number;
 }
 
-async function pollingBroker(ctx: RunnerContext): Promise<never> {
-  // Catch and log errors when polling the broker
-  try {
-    // Craft the url to the broker
-    const jobs_url = new URL('api/jobs', ctx.brokerUrl);
-    jobs_url.searchParams.append('os', ctx.fiddleExecPath);
-
-    // Make the request
-    const jobs: IncomingJob[] = await got(jobs_url).json();
-
-    // Transition to the Recieved Jobs state
-    return receivedJobs(ctx, jobs);
-  } catch (err) {
-    d('error while polling broker: %O', err);
-
-    // Transition to the Timeout state on errors for resilience
-    // TODO(clavin): capped exponential backoff + jitter?
-    return timeout(ctx);
-  }
+interface BisectJob extends BaseJob {
+  type: 'bisect';
+  first: string;
+  last: string;
+  result_bisect?: [string, string];
 }
 
-function timeout(ctx: RunnerContext): Promise<never> {
-  // We need to wrap `setTimeout` in a Promise
+interface TestJob extends BaseJob {
+  type: 'test';
+  version: string;
+}
+
+type AnyJob = BisectJob | TestJob;
+
+type JsonPatch = unknown;
+
+/**
+ * Returns a promise that resolves after the specified timeout.
+ */
+function timeout(ms: number): Promise<void> {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      // Resolve by transitioning to the Polling Broker state
-      resolve(pollingBroker(ctx));
-    }, ctx.pollTimeoutMs);
+    setTimeout(() => resolve(), ms);
   });
 }
 
-function receivedJobs(ctx: RunnerContext, jobs: IncomingJob[]): Promise<never> {
-  // Filter out claimed jobs
-  const unclaimedJobs = jobs.filter((job) => !('runner' in job));
+class Runner {
+  private readonly uuid: string;
+  private readonly fiddleExecPath: string;
+  private readonly brokerUrl: string;
+  private readonly osFilter: string;
+  private readonly pollTimeoutMs: number;
 
-  // Check if there were any unclaimed jobs
-  if (unclaimedJobs.length === 0) {
-    // Transition to the Timeout state
-    return timeout(ctx);
+  /**
+   * Creates and initializes the runner from environment variables and default
+   * values, then starts the runner's execution loop.
+   */
+  static start(): Promise<never> {
+    // Determine the OS filter from the current running platform
+    let osFilter = '';
+    switch (process.platform) {
+      case 'darwin':
+        osFilter = 'mac';
+        break;
+      case 'linux':
+        osFilter = 'linux';
+        break;
+      case 'win32':
+        osFilter = 'windows';
+        break;
+      default:
+        d('Cannot detect the current operating system, exiting.');
+        return null;
+    }
+
+    // Create the runner
+    const runner: Runner = Object.create(Runner.prototype, {
+      brokerUrl: {
+        value: env('BUGBOT_BROKER_URL'),
+      },
+      fiddleExecPath: {
+        value: env('FIDDLE_EXEC_PATH'),
+      },
+      osFilter: {
+        value: osFilter,
+      },
+      pollTimeoutMs: {
+        value: 20 * 1000, // 20 seconds
+      },
+      uuid: {
+        value: uuidv4(),
+      },
+    });
+
+    // Begin running the poll loop
+    return runner.pollLoop();
   }
 
-  // Claim the first job
-  // TODO(clavin): would adding some jitter (like "randomly claim first or second job") help reduce
-  // possible contention?
-  return claimingJob(ctx, unclaimedJobs[0]);
-}
+  async pollLoop(): Promise<never> {
+    // Wrap the loop in a try-catch to log errors and make the runner resilient
+    try {
+      // Check for any claimable jobs
+      const claimableJobs = await this.fetchUnclaimedJobs();
 
-async function claimingJob(
-  ctx: RunnerContext,
-  job: IncomingJob,
-): Promise<never> {
-  // Catch and log errors when claiming the job
-  try {
-    // Tell the broker we claim this job
-    const claim_url = new URL(`api/jobs/${job.id}`, ctx.brokerUrl);
-    await got(claim_url, {
-      json: [
+      // If there are no unclaimed jobs then sleep and try again
+      if (claimableJobs.length === 0) {
+        await timeout(this.pollTimeoutMs);
+        return this.pollLoop();
+      }
+
+      // Otherwise, claim the first job available
+      const [jobId] = claimableJobs;
+      // TODO(clavin): would adding jitter (e.g. claim first OR second randomly)
+      // help reduce any possible contention?
+      let etag = '';
+      const [job, initalEtag] = await this.fetchJobAndEtag(jobId);
+      etag = initalEtag;
+
+      // Claim the job
+      etag = await this.patchJobAndUpdateEtag(job.id, etag, [
         {
           op: 'add',
           path: '/runner',
-          value: ctx.uuid,
+          value: this.uuid,
         },
         {
           op: 'add',
           path: '/time_started',
-          value: Date.now() / 1000,
+          value: Date.now(),
         },
-      ],
+      ]);
+
+      // Another layer of catching errors to also unclaim the job if we error
+      try {
+        // Then determine how to run the job and return results
+        if (job.type === 'bisect') {
+          // Run the bisect
+          const result = await this.runBisect(job.first, job.last, job.gist);
+
+          // Report the result back to the job
+          if (result.success) {
+            etag = await this.patchJobAndUpdateEtag(job.id, etag, [
+              {
+                op: 'add',
+                path: '/time_finished',
+                value: Date.now(),
+              },
+              {
+                op: 'add',
+                path: '/result_bisect',
+                value: [result.goodVersion, result.badVersion],
+              },
+            ]);
+          } else {
+            etag = await this.patchJobAndUpdateEtag(job.id, etag, [
+              {
+                op: 'add',
+                path: '/time_finished',
+                value: Date.now(),
+              },
+              {
+                op: 'add',
+                path: '/error',
+                value: 'Failed to narrow test down to two versions',
+                // TODO(clavin): ^ better wording
+              },
+            ]);
+          }
+
+          // } else if (job.type === 'test') {
+          // TODO
+        } else {
+          throw new Error(`unexpected job type: "${(job as AnyJob).type}"`);
+        }
+      } catch (err) {
+        // Unclaim the job and rethrow
+        await this.patchJobAndUpdateEtag(job.id, etag, [
+          {
+            op: 'remove',
+            path: '/runner',
+            value: this.uuid,
+          },
+          {
+            op: 'remove',
+            path: '/time_started',
+            value: Date.now(),
+          },
+        ]);
+        throw err;
+      }
+    } catch (err) {
+      d('error while polling broker: %O', err);
+    }
+
+    // Sleep and then try again
+    await timeout(this.pollTimeoutMs);
+    return this.pollLoop();
+  }
+
+  /**
+   * Polls the broker for a list of unclaimed job IDs.
+   */
+  private async fetchUnclaimedJobs(): Promise<string[]> {
+    // Craft the url to the broker
+    const jobs_url = new URL('api/jobs', this.brokerUrl);
+    jobs_url.searchParams.append('os', this.fiddleExecPath);
+    jobs_url.searchParams.append('runner', 'undefined');
+
+    // Make the request and return its response
+    return await got(jobs_url).json();
+  }
+
+  private async fetchJobAndEtag(id: string): Promise<[AnyJob, string]> {
+    const job_url = new URL(`api/jobs/${id}`, this.brokerUrl);
+    const resp = await got(job_url);
+
+    // Extract the etag header & make sure it was defined
+    const { etag } = resp.headers;
+    if (!etag) {
+      throw new Error('missing etag in broker job response');
+    }
+
+    return [JSON.parse(resp.body), etag];
+  }
+
+  private async patchJobAndUpdateEtag(
+    id: string,
+    etag: string,
+    patches: JsonPatch[],
+  ): Promise<string> {
+    // Send the patch
+    const job_url = new URL(`api/jobs/${id}`, this.brokerUrl);
+    const resp = await got(job_url, {
+      headers: { etag },
+      json: patches,
       method: 'PATCH',
     });
 
-    // If we got here then assume the claim was successful
-    return executingJob(ctx, job);
-  } catch (err) {
-    d('error while claiming job: %O', err);
+    // Extract the etag header & make sure it was defined
+    const newEtag = resp.headers.etag;
+    if (!newEtag) {
+      throw new Error('missing etag in broker job response');
+    }
 
-    // Transition to the Timeout state for resilience
-    return timeout(ctx);
+    return newEtag;
   }
-}
 
-function executingJob(ctx: RunnerContext, job: IncomingJob): Promise<never> {
-  // Route to the proper execution state
-  if (job.type === 'bisect') {
-    return executingBisect(ctx, job);
-  }
-  // } else if (job.type === 'test') {
-  //   return executingTest(ctx, job);
-  // }
-
-  // TODO(clavin): resilience
-  throw new Error(
-    `encountered unknown job type "${(job as IncomingJob).type}"`,
-  );
-}
-
-function executingBisect(
-  ctx: RunnerContext,
-  job: IncomingJob & { type: 'bisect' },
-): Promise<never> {
-  // Alias some of the job arguments
-  const goodVersion = job.first;
-  const badVersion = job.last;
-  const gistId = job.gist;
-
-  // Call fiddle and instruct it to bisect with the supplied parameters
-  return new Promise((resolve, reject) => {
-    execFile(
-      ctx.fiddleExecPath,
-      ['bisect', goodVersion, badVersion, '--fiddle', gistId] as string[],
-      (err, stdout, stderr) => {
-        // Ensure there was no error
-        if (err === null) {
-          try {
-            // Try to parse the output as well
-            const result = parseFiddleBisectOutput(stdout);
-
-            // Transition to the Bisect Finished state if it worked
-            resolve(bisectFinished(ctx, job, result));
-          } catch (parseErr) {
-            // TODO(clavin): resilience
-            reject(parseErr);
+  private runBisect(
+    goodVersion: string,
+    badVersion: string,
+    gistId: string,
+  ): Promise<FiddleBisectResult> {
+    // Call fiddle and instruct it to bisect with the supplied parameters
+    return new Promise((resolve, reject) => {
+      execFile(
+        this.fiddleExecPath,
+        ['bisect', goodVersion, badVersion, '--fiddle', gistId] as string[],
+        (err, stdout, stderr) => {
+          // Ensure there was no error
+          if (err === null) {
+            try {
+              // Try to parse the output as well
+              resolve(parseFiddleBisectOutput(stdout));
+            } catch (parseErr) {
+              d('fiddle bisect parse error: %O', parseErr);
+              reject(parseErr);
+            }
+          } else {
+            d(`failed fiddle bisect stdout:\n${stdout}`);
+            d(`failed fiddle bisect stderr:\n${stderr}`);
+            reject(err);
           }
-        } else {
-          // TODO(clavin): resilience
-          d(`failed fiddle stdout:\n${stdout}`);
-          d(`failed fiddle stderr:\n${stderr}`);
-          reject(err);
-        }
-      },
-    );
-  });
-}
-
-async function bisectFinished(
-  ctx: RunnerContext,
-  job: IncomingJob & { type: 'bisect' },
-  result: FiddleBisectResult,
-): Promise<never> {
-  // Notify the broker of our result
-  const update_url = new URL(`api/jobs/${job.id}`, ctx.brokerUrl);
-  await got(update_url, {
-    json: [
-      {
-        op: 'remove',
-        path: '/runner',
-      },
-      {
-        op: 'add',
-        path: '/time_finished',
-        value: Date.now() / 1000,
-      },
-      {
-        op: 'add',
-        path: '/result_bisect',
-        value: result.success ? [result.goodVersion, result.badVersion] : [],
-      },
-    ],
-    method: 'PATCH',
-  });
-
-  // If we got this far that means the update was successful
-
-  // Transition to the Timeout state
-  return timeout(ctx);
-}
-
-// async function executingTest(ctx: RunnerContext, job: IncomingJob & { type: 'test' }): Promise<never> {
-//   //
-// }
-
-// async function testFinished(ctx: RunnerContext, job: IncomingJob & { type: 'test' }): Promise<never> {
-//   // TODO
-// }
-
-function runner(): Promise<never> {
-  // Determine the OS filter from the current running platform
-  let osFilter = '';
-  switch (process.platform) {
-    case 'darwin':
-      osFilter = 'mac';
-      break;
-    case 'linux':
-      osFilter = 'linux';
-      break;
-    case 'win32':
-      osFilter = 'windows';
-      break;
-    default:
-      d('Cannot detect the current operating system, exiting.');
-      return;
+        },
+      );
+    });
   }
-
-  // Create the runner context
-  const ctx: RunnerContext = {
-    brokerUrl: env('BUGBOT_BROKER_URL'),
-    fiddleExecPath: env('FIDDLE_EXEC_PATH'),
-    osFilter,
-    pollTimeoutMs: 20 * 1000, // 20 seconds
-    uuid: uuidv4(),
-  };
-
-  // Begin the machine in the Polling Broker state
-  pollingBroker(ctx);
 }
 
-// Run the runner as an asynchronous loop, handling any errors that arise
-runner().catch((err) => {
+// Start the runner and catch any errors that bubble up
+Runner.start().catch((err) => {
   d('encountered an error: %O', err);
+  console.error('execution stopped due to a critical error');
   process.exit(1);
 });
--- modules/shared/src/env-vars.ts
+++ modules/shared/src/env-vars.ts
@@ -7,12 +7,13 @@ const d = debug('env-vars');
  * if the requested variable wasn't found.
  */
 export function env(name: string): string {
-  if (!Object.prototype.hasOwnProperty.call(process.env, name)) {
+  if (Object.prototype.hasOwnProperty.call(process.env, name)) {
     return process.env[name];
   }
 
   d(`Could not find environment variable "${name}"`);
   process.exit(1);
+
   // This line is never reached and only here to appease the linter
   return '' as never;
 }
