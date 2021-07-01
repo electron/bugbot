7c759f71a5d4cf9fb1d54458f9097d091e72d0a4
Charles Kerr <charles@charleskerr.com>
2021-07-01 12:37:21 -0500

    test: add typescript-eslint-recommended-requiring-type-checking.
    Also, disable most of its warnings because there are quite a few.
    They can be re-enabled piecemeal.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+const config = {
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: [
+      './integration-tests/tsconfig.json',
+      './modules/bot/tsconfig.json',
+      './modules/broker/tsconfig.json',
+      './modules/runner/tsconfig.json',
+      './modules/shared/tsconfig.json',
+      './tsconfig.json',
+    ],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-implied-eval': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/prefer-regexp-exec': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+  }
+}
+
+module.exports = config;
