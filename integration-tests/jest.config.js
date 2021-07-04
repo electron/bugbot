33a8d75d4ab67e0bf720e5e12e8a8ac8fc6ad5e3
Charles Kerr <charles@charleskerr.com>
2021-07-04 10:18:01 -0500

    test: include integration tests in test coverage

--- integration-tests/jest.config.js
+++ integration-tests/jest.config.js
@@ -2,8 +2,8 @@ process.env.SPEC_RUNNING = '1';
 
 module.exports = {
   clearMocks: true,
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   preset: 'ts-jest',
-  roots: ['<rootDir>/test'],
-  testRegex: '(/spec/.*|(\\.|/)(test|spec))\\.tsx?$',
+  rootDir: `${__dirname}/..`,
+  roots: [`${__dirname}/test`],
+  testMatch: ['**/*.(spec|test).ts'],
 };
