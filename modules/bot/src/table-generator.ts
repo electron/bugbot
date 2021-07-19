efabf6293dc230df84d33b955bd5956d567f8ec3
Charles Kerr <charles@charleskerr.com>
2021-07-19 11:36:47 -0500

    feat: prettify the test matrix markdown a bit. (#133)
    * feat: prettify the test matrix markdown a bit.

--- modules/bot/src/table-generator.ts
+++ modules/bot/src/table-generator.ts
@@ -1,5 +1,6 @@
-import { TestJob } from '@electron/bugbot-shared/src/interfaces';
+import { Platform, TestJob } from '@electron/bugbot-shared/src/interfaces';
 import { debug } from 'debug';
+import { URL } from 'url';
 import table from 'markdown-table';
 
 // FIXME(any): better typing here
@@ -9,34 +10,65 @@ export interface Matrix {
   win32?: any;
 }
 
-export function generateTable(jobMatrix: Matrix) {
-  const d = debug('generateTable');
+function cellLabel(job: TestJob) {
+  switch (job.last?.status) {
+    case 'success':
+      return 'Passed&ensp;🟢';
+    case 'failure':
+      return 'Failed&ensp;🔴';
+    case 'system_error':
+      return 'Error&ensp;🟠';
+    case 'test_error':
+      return 'Test Error&ensp;🔵';
+    default:
+      return 'Pending&ensp;🟡';
+  }
+}
+
+function platformDisplayName(platform: Platform) {
+  switch (platform) {
+    case 'darwin':
+      return 'macOS';
+    case 'linux':
+      return 'Linux';
+    case 'win32':
+      return 'Windows';
+  }
+}
+
+function generateCell(job: TestJob, brokerBaseUrl: string) {
+  const logUrl = new URL(`/log/${job.id}`, brokerBaseUrl);
+  return `[${cellLabel(job)}](${logUrl.toString()})`;
+}
+
+export function generateTable(jobMatrix: Matrix, brokerBaseUrl: string) {
+  const d = debug('bot:generateTable');
   d('input matrix %O', jobMatrix);
 
-  const platforms = Object.keys(jobMatrix);
+  const platforms = Object.keys(jobMatrix) as Platform[];
   // this assumes that each platform tests the same version list
   const versions = Object.keys(jobMatrix[platforms[0]]);
 
   const tableInput: string[][] = [];
 
   // header row
-  const header = ['', ...platforms];
+  const header = ['Electron', ...platforms.map((p) => platformDisplayName(p))];
   tableInput.push(header);
 
   // each row is a version with its results per platform
   for (const version of versions) {
     const versionRow = [version];
 
-    // TODO(any): prettify the status messages
     for (const plat of platforms) {
       const result = jobMatrix[plat][version] as TestJob;
-      versionRow.push(result?.last.status ? result.last.status : '-');
+      versionRow.push(generateCell(result, brokerBaseUrl));
     }
 
     tableInput.push(versionRow);
   }
 
-  const tableOutput = table(tableInput);
+  const align = ['l', ...new Array(platforms.length).fill('r')];
+  const tableOutput = table(tableInput, { align });
   d(tableOutput);
   return tableOutput;
 }
