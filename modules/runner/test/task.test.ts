17443e79a75b7002a34bb020523a74277abec116
Charles Kerr <charles@charleskerr.com>
2021-08-06 15:42:04 -0500

    chore: fix tab damage

--- modules/runner/test/task.test.ts
+++ modules/runner/test/task.test.ts
@@ -1,23 +1,23 @@
 import { Task } from '../src/task';
 
 describe('Task', () => {
-	describe('claimForRunner()', () => {
-		it.todo('tries to patch the job on the broker');
-		it.todo('sends an etag in its header');
-		it.todo('returns true if the patch succeeded');
-		it.todo('returns false if the patch failed');
-	});
+  describe('claimForRunner()', () => {
+    it.todo('tries to patch the job on the broker');
+    it.todo('sends an etag in its header');
+    it.todo('returns true if the patch succeeded');
+    it.todo('returns false if the patch failed');
+  });
 
-	describe('sendResult()', () => {
-		it.todo('appends job.history with the result');
-		it.todo('assigns job.last with the result');
-		it.todo('unsets job.current to indicate no current runner');
-		it.todo('sends an etag in its header');
-		it.todo('returns true if the patch succeeded');
-		it.todo('returns false if the patch false');
-	});
+  describe('sendResult()', () => {
+    it.todo('appends job.history with the result');
+    it.todo('assigns job.last with the result');
+    it.todo('unsets job.current to indicate no current runner');
+    it.todo('sends an etag in its header');
+    it.todo('returns true if the patch succeeded');
+    it.todo('returns false if the patch false');
+  });
 
-	describe('addLogData()', () => {
-		it.todo('eventually is sent to the broker');
-	});
+  describe('addLogData()', () => {
+    it.todo('eventually is sent to the broker');
+  });
 });
